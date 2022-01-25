5cb8d3923a3483f881ba6b0b75fa8e0b5984218d
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-26 00:56:48 +0200

    user and lesson data seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Lesson;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\LessonSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $lessons = Lesson::factory()
-            ->count(20)
-            ->create();
+        $this->call([
+            UserSeeder::class,
+            LessonSeeder::class,
+        ]);
     }
 }
