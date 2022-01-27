2945c2df5dfa75dff1ff1b2696412d3e383d8390
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-27 10:26:51 +0200

    refactor/write some comments

--- app/Traits/Achiever.php
+++ app/Traits/Achiever.php
@@ -8,12 +8,25 @@ use App\Traits\EntityRelationsAchievements;
 trait Achiever
 {
     use EntityRelationsAchievements, EntityRelationsBadges;
-
+    
+    /**
+     * addProgress
+     * adding achievement in progress.
+     * @param  mixed $instance
+     * @param  mixed $points
+     * @return void
+     */
     public function addProgress($instance, $points)
     {
         $instance->addProgressToAchiever($this, $points);
     }
-
+    
+    /**
+     * unlock
+     * unlock achievements directly.
+     * @param  mixed $instance
+     * @return void
+     */
     public function unlock($instance)
     {
         $this->addProgress($instance, $instance->points);
