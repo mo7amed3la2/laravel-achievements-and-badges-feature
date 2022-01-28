53e7b61c141eef807f5f9b9fd22053043d147610
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-28 21:15:08 +0200

    first achievement test unlocked

---
+++ tests/Unit/AchievementTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use App\Achievements\Comments\FirstCommentWritten;
+use App\Achievements\Comments\FiveCommentsWritten;
+use App\Models\Achievement;
+use App\Models\AchievementProgress;
+
+class AchievementTest extends TestCase
+{
+    public $users;
+    public $firstCommentWritten;
+    public $fiveCommentsWritten;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh');
+        Artisan::call('migrate');
+        $this->user = User::factory()->create();
+
+        $this->firstCommentWritten = new FirstCommentWritten();
+        $this->fiveCommentsWritten = new FiveCommentsWritten();
+    }
+
+    /**
+     * Test unlocked achievement.
+     *
+     * @return void
+     */
+    public function test_unlocked()
+    {
+        // check user does not have achievements.
+        $this->assertEquals(0, $this->user->achievements->count());
+
+        // unlock first achievement.
+        $this->user->unlock($this->firstCommentWritten);
+        $this->user = $this->user->fresh();
+
+        // check user has one unlocked achievement.
+        $this->assertEquals(1, $this->user->unlockedAchievements()->count());
+    }
+
+
+}
