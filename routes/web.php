9d05fb43527077ea5b62c89a89f2290ef3808b04
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-28 20:31:07 +0200

    code refactor

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,15 @@
 use App\Models\User;
 use App\Models\Lesson;
 use App\Models\Comment;
-use App\Models\Achievement;
 use App\Events\LessonWatched;
 use App\Events\CommentWritten;
-use App\Models\AchievementProgress;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AchievementsController;
 
 Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
 
 
-Route::get('/', function () {
+Route::get('/fire-events', function () {
     $user = User::first();
 
     Lesson::factory()
@@ -31,15 +29,4 @@ Route::get('/', function () {
 
     $comment = Comment::latest()->first();
     CommentWritten::dispatch($comment);
-});
-
-
-Route::get('/test', function () {
-
-
-    $lesson = new Lesson(array('title' => 'A new lesson.'));
-
-    $user = User::find(1);
-
-    $lesson = $user->lessons()->attach([2 => ['watched' => true]]);
 });
