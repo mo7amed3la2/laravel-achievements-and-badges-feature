6303940ca5fdb4417fe23ddb8a9bf33e65af6a7f
Mohamed Alaa <m.3laa.95@gmail.com>
2022-01-28 06:29:28 +0200

    adding code comments

--- app/Traits/EntityRelationsAchievements.php
+++ app/Traits/EntityRelationsAchievements.php
@@ -12,27 +12,44 @@ trait EntityRelationsAchievements
         return $this->hasMany(AchievementProgress::class);
     }
 
+    /**
+     * In progress achievements.
+     */
     public function inProgressAchievements()
     {
         return $this->achievements()->whereNull('unlocked_at')->where('points', '>', 0)->get();
     }
 
+    /**
+     * Unlocked achievements.
+     */
     public function unlockedAchievements()
     {
-        return $this->achievements()->with('achievement')->whereNotNull('unlocked_at')->orderBy('points','ASC')->get();
+        return $this->achievements()->with('achievement')->whereNotNull('unlocked_at')->orderBy('points', 'ASC')->get();
     }
 
+    /**
+     * Locked achievements.
+     */
     public function lockedAchievements()
     {
         return $this->achievements()->with('achievement')->whereNull('unlocked_at')->get();
     }
 
 
+    /**
+     * List unlocked achievements.
+     */
     public function listUnlockedAchievements()
     {
         return $this->unlockedAchievements()->pluck('achievement.name');
     }
 
+    /**
+     * List next available achievements.
+     *
+     * @return array
+     */
     public function nextAvailableAchievements()
     {
         $data = [];
@@ -42,7 +59,7 @@ trait EntityRelationsAchievements
         foreach ($achievementTypes as $type) {
             $achievement = AchievementProgress::whereHas('achievement', function ($q) use ($type) {
                 $q->where('type', $type);
-            })->whereNull('unlocked_at')->orderBy('points','ASC')->first();
+            })->whereNull('unlocked_at')->orderBy('points', 'ASC')->first();
 
             if ($achievement) {
                 $data[] = $achievement->achievement->name;
--- app/Traits/EntityRelationsBadges.php
+++ app/Traits/EntityRelationsBadges.php
@@ -4,7 +4,6 @@ namespace App\Traits;
 
 use App\Models\Badge;
 use App\Badges\Beginner;
-use App\Badges\Intermediate;
 use App\Models\BadgeProgress;
 
 trait EntityRelationsBadges
@@ -14,35 +13,55 @@ trait EntityRelationsBadges
         return $this->hasMany(BadgeProgress::class);
     }
 
+    /**
+     * In progress badges.
+     */
     public function inProgressBadges()
     {
         return $this->badges()->whereNull('unlocked_at')->where('points', '>', 0)->get();
     }
 
+    /**
+     * Unlocked badges.
+     */
     public function unlockedBadges()
     {
-        return $this->badges()->with('badge')->whereNotNull('unlocked_at')->orderBy('points','ASC')->get();
+        return $this->badges()->with('badge')->whereNotNull('unlocked_at')->orderBy('points', 'ASC')->get();
     }
 
+    /**
+     * Locked badges.
+     */
     public function lockedBadges()
     {
         return $this->badges()->with('badge')->whereNull('unlocked_at')->get();
     }
 
+    /**
+     * List unlocked badges.
+     */
     public function listUnlockedBadges()
     {
         return $this->unlockedBadges()->pluck('badge.name');
     }
 
+    /**
+     * Current badge
+     */
     public function currentBadge()
     {
-        $currentBadge = $this->badges()->whereNotNull('unlocked_at')->where('points', '>', 0)->orderBy('id','DESC')->get()->first();
+        $currentBadge = $this->badges()->whereNotNull('unlocked_at')->where('points', '>', 0)->orderBy('id', 'DESC')->get()->first();
         if ($currentBadge) {
             return $currentBadge->badge->description;
         }
         return (new Beginner)->description;
     }
 
+    /**
+     * Next available badge
+     *
+     * @return string
+     */
     public function nextAvailableBadge()
     {
         $nextAvailableBadge = BadgeProgress::whereHas('badge', function ($q) {
@@ -52,9 +71,13 @@ trait EntityRelationsBadges
         if ($nextAvailableBadge) {
             return $nextAvailableBadge->badge->description;
         }
-        return (new Intermediate)->description;
+        return '';
     }
 
+    /**
+     * Remaing to unlock next badge.
+     * @return integer
+     */
     public function remaingToUnlockNextBadge()
     {
         $nextAvailableBadge = BadgeProgress::whereHas('badge', function ($q) {
@@ -63,7 +86,7 @@ trait EntityRelationsBadges
 
         if ($nextAvailableBadge) {
             $badgePoints = $nextAvailableBadge->badge->points;
-            return $badgePoints - $nextAvailableBadge->points ;
+            return $badgePoints - $nextAvailableBadge->points;
         }
         return 0;
     }
